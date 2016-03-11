09b2d47dbc1124dca6aee584659638b08a5f0911
irisbune <irisbune@gmail.com>
2016-03-11 13:41:43 +0100

    defined model spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
 
-  
+  it "is valid with a name, description and start date"
+  it "is invalid without a name"
+  it "is invalid without a start date"
+  it "cannot have a start date in the past"
+  it "has a valid date when the format is mm/dd/yyyy"
+  it "has an has_many association with tasks"
+
 end
