16aadb27086c6bd1a363c85c69e1882ddc931c36
irisbune <irisbune@gmail.com>
2016-03-16 13:07:07 +0100

    added validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,48 +2,54 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
 
-  describe "validations" do
-    it { is_expected.to validate_presence_of(:task_description) }
-    it { is_expected.to validate_presence_of(:duedate) }
-    # it { is_expected.to validate_presence_of(:priority) }
-    # it { is_expected.to validate_presence_of(:status) }
-  end
-
-  # describe "allowed values for priority" do
-  #   it { should validate_inclusion_of(:priority). in_array(['high', 'normal', 'low']) }
+  # describe "validations" do
+  #   it { is_expected.to validate_presence_of(:task_description) }
+  #   it { is_expected.to validate_presence_of(:duedate) }
+  # end
+  #
+  # describe "allowed values for duedate" do
+  #   it { should allow_value(Date.tomorrow).for(:duedate) }
+  #   it { should_not allow_value(1.days.ago).for(:duedate) }
   # end
 
-  describe "allowed values for duedate" do
-    it { should allow_value(Date.tomorrow).for(:duedate) }
-    it { should_not allow_value(1.days.ago).for(:duedate) }
+  describe "validations" do
+    it "has a valid Factory" do
+      expect(build(:task)).to be_valid
+    end
+
+    it "is invalid without a task description" do
+      task = build(:task, task_description: nil)
+      task.valid?
+      expect(task.errors[:task_description]).to include("can't be blank")
+    end
+
+    it "is invalid without a duedate" do
+      task = build(:task, duedate: nil)
+      task.valid?
+      expect(task.errors[:duedate]).to include("can't be blank")
+    end
+
+    it "is invalid with a duedate in the past" do
+      task = build(:task, duedate: Date.yesterday)
+      task.valid?
+      expect(task.errors[:duedate]).to include("can't be in the past")
+    end
+
+    it "is invalid without a status boolean" do
+      task = build(:task, status: nil)
+      task.valid?
+      expect(task.errors[:status]).to include("can't be blank or anything other than true/false")
+    end
+
+    it "is invalid without a project" do
+      task = build(:task, project_id: 400)
+      task.valid?
+      expect(task.errors[:project_id]).to include("Project does not exist")
+    end
+
   end
 
-# it "accepts only a valid date" do
-#
-# date = "2000-11-11"
-# task = Task.new duedate: date
-# task.should be_valid
-# task.duedate.should == Date.parse(date)
-#
-# end
-
-#   describe "allow value of status to be true or false!!" do
-#   # This will work, but will give you a warning
-#   it {should validate_inclusion_of(:status).in_array([true, false])}
-# end
-
-
-
-#   RSpec::Matchers.define :be_date_with_our_format do |expected|
-#   match do |date|
-#     date =~ /\d{4}.\d{2}.\d{2}/
-#   end
-# end
-#
-# it "has a placed_at date" do
-#   bid.placed_at.should be_datetime_with_our_format
-# end
-
-
+  it "has a status summary"
+  it "has a due date filter"
 
 end
